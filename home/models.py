9119ae95ec7c3a9a15e99c24bc35ace400cd64fc
Ritik <ritik09052000@gmail.com>
2020-07-05 21:42:46 +0530

    handled contact form in views and used messeges to handle invalid form

--- home/models.py
+++ home/models.py
@@ -10,3 +10,6 @@ class Contact(models.Model):
     email = models.EmailField(max_length=100)
     content = models.TextField()
     timeStamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return 'Message from :' + self.name
--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Contact
+from django.contrib import messages
 # Create your views here.
 
 
@@ -9,8 +10,23 @@ def home(request):
 
 
 def contact(request):
+    messages.info(request, 'Welcome To contacts')
+    if(request.method == 'POST'):
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        content = request.POST.get('content', '')
+        if(len(name) < 2 or len(email) < 3 or len(phone) < 10 or len(content) < 5):
+            messages.error(request, "Please fill the form details correctly! ")
+        else:
+            contact = Contact(name=name, email=email,
+                              phone=phone, content=content)
+            contact.save()
+            messages.success(
+                request, "Your message has been Successfully sent")
     return render(request, 'home/contact.html')
 
 
 def about(request):
+    messages.success(request, 'Welcome To abouts')
     return render(request, 'home/about.html')
