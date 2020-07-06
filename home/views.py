8b298bab8e7a26ea075d619ace81357887ec8bf3
Ritik <ritik09052000@gmail.com>
2020-07-06 06:02:20 +0530

    created and added blogs

--- home/views.py
+++ home/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Contact
 from django.contrib import messages
+from blog.models import Post
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home/home.html')
+    allPosts = Post.objects.all()
+    print(allPosts)
+    context = {'allPosts': allPosts}
+    return render(request, 'home/home.html', context)
 
 
 def contact(request):
