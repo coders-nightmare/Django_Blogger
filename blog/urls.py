98330bffce3162e6891c9c6eeefa5241d1b0aa91
Ritik <ritik09052000@gmail.com>
2020-07-08 21:57:50 +0530

    added comment feature in blogPost

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('postComment/', views.postComment, name="postComment"),
     path('', views.blogHome, name="blogHome"),
+    # Api to post commnet
     path('<str:slug>/', views.blogPost, name="blogPost"),
 ]
