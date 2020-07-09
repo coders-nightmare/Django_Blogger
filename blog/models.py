83c5f2e8f77fce2f4618a0c3f653d7a998fd4cbb
Ritik <ritik09052000@gmail.com>
2020-07-09 21:07:16 +0530

    improved comments and disabled for anonymous users

--- blog/models.py
+++ blog/models.py
@@ -19,6 +19,7 @@ class Post(models.Model):
 class BlogComment(models.Model):
     sno = models.AutoField(primary_key=True)
     comment = models.TextField()
+    # foriegn key creates link to refered models so they can use there class fields also
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
